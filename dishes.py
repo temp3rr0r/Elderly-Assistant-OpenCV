c1f0ad9c5e5d588072663054f3a0118f23491931
temp3rr0r <madks@yahoo.gr>
2017-07-15 23:03:25 +0000

    Added circle detection and count

--- dishes.py
+++ dishes.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 #calculate the average brightness of an image
 #used to work out if its night time and turn the alarms off
@@ -21,9 +22,22 @@ def main():
 		exit()
 
 	thresholdCanny = 200
-	grayScale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # TODO: Create grayscale image
-	edges = cv2.Canny(grayScale, thresholdCanny, thresholdCanny /2, 3) # TODO: edge detection
-	smoothEdges = cv2.GaussianBlur(grayScale, (3, 3), 0)# TODO: smooth edges
+	grayScale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Create grayscale image
+	edges = cv2.Canny(grayScale, thresholdCanny, thresholdCanny /2, 3) # Edge detection
+	smoothEdges = cv2.GaussianBlur(grayScale, (3, 3), 0)# Smooth edges
+	#houghCircles = cv2.HoughCircles(grayScale, cv2.HOUGH_GRADIENT, 2, grayScale.width / 18, thresholdCanny, 300, 0, 0)# TODO: Find hough circles
+
+	circles = cv2.HoughCircles(grayScale, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=thresholdCanny, minRadius=0, maxRadius=0)
+
+	if circles is not None:
+		print "Circles found: " + str(len(circles))
+		print "Circles array: "
+		print circles
+
+		circles = np.uint16(np.around(circles))
+		for i in circles[0,:]:
+			cv2.circle(grayScale,(i[0],i[1]),i[2],(0,255,0),2) # draw the outer circle
+			cv2.circle(grayScale,(i[0],i[1]),2,(0,0,255),3) # draw the center of the circle
 
 	cv2.imwrite('grayScale.png', grayScale) # Store frame as png
 	cv2.imwrite('edges.png', edges) # Store frame as png
