11bb64281e1f0b8fd402a1ec8b71f68a0a005a56
temp3rr0r <madks@yahoo.gr>
2017-07-16 01:24:47 +0000

    Before making a python service & before creating the barebones express api restfull results

--- dishes.py
+++ dishes.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 import datetime
 import sqlite3
+import time
+
+sleepInterval = 60 * 60 # 60 minutes
 
 def saveDishesCountDb(brightness, dishes):
 	conn = sqlite3.connect('dishes.db')
@@ -18,45 +21,46 @@ def getMeanBrightness(img):
 
 def main():
 	
-        cap = cv2.VideoCapture(-1) # Load default video capture device
-	ret, frame = cap.read() # Load one frame from capture device
+	while True: # Loop forever, sleep every sleepInterval
+	        cap = cv2.VideoCapture(-1) # Load default video capture device
+		ret, frame = cap.read() # Load one frame from capture device
 	
-	meanBrightness = getMeanBrightness(frame)
-	#print("Mean Brightness: " + str(meanBrightness))
+		meanBrightness = getMeanBrightness(frame)
+		#print("Mean Brightness: " + str(meanBrightness))
 
-	if meanBrightness < 30:
-		pring("Exiting, too low brightness")
-		exit()
+		if meanBrightness < 30:
+			pring("Exiting, too low brightness")
+			exit()
 
-	thresholdCanny = 200
-	grayScale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Create grayscale image
-	grayScale = cv2.GaussianBlur(grayScale, (9, 9), 0) # Smooth edges	
-	circles = cv2.HoughCircles(grayScale, cv2.HOUGH_GRADIENT, \
-		# Inverse ratio of resolution
-		 1,\
-		# Minimum distance between centers
-		20,\
-		# Upper threshold for the internal Canny Edge detector
-		param1=200, \
-		# Threshold for center detection
-		param2=100, \
-		minRadius=0, \
-		maxRadius=0)
+		thresholdCanny = 200
+		grayScale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Create grayscale image
+		grayScale = cv2.GaussianBlur(grayScale, (9, 9), 0) # Smooth edges	
+		circles = cv2.HoughCircles(grayScale, cv2.HOUGH_GRADIENT, \
+			# Inverse ratio of resolution
+			 1,\
+			# Minimum distance between centers
+			20,\
+			# Upper threshold for the internal Canny Edge detector
+			param1=200, \
+			# Threshold for center detection
+			param2=100, \
+			minRadius=0, \
+			maxRadius=0)
 
-	if circles is not None:
-		#print "Circles found: " + str(len(circles))
-		#print "Circles array: "
-		#print circles
-		saveDishesCountDb(meanBrightness, len(circles))
+		if circles is not None:
+			#print "Circles found: " + str(len(circles))
+			#print "Circles array: "
+			#print circles
+			saveDishesCountDb(meanBrightness, len(circles))
 
-		circles = np.uint16(np.around(circles))
-		for i in circles[0,:]:
-			cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2) # draw the outer circle
-			cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3) # draw the center of the circle
+			circles = np.uint16(np.around(circles))
+			for i in circles[0,:]:
+				cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),2) # draw the outer circle
+				cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3) # draw the center of the circle
 
-	cv2.imwrite('frame.png', frame) # Store frame as png
+		cv2.imwrite('frame.png', frame) # Store frame as png
 
-	cap.release()
-	cv2.destroyAllWindows()
-
+		cap.release()
+		cv2.destroyAllWindows()
+		time.sleep(sleepInterval)
 main()
